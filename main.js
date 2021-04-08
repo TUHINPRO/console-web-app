249fdb2223201f57b30ae64409499347a2d00ed3
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-04-08 19:22:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+name=TUHIN;
+console.log(merge3);
+var merge1="Tuhin";
+var merge2="Biswas";
+var merge3=merge1.concat(merge2); 
+ 
+console.log(numbermerge);
+
+var number1="5"
+var number2="5"
+var numbermerge=number1.concat(number2);
